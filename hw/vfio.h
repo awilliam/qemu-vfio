d251ba30501e028433ba925088dfeb5bd1bba5bd
Alex Williamson <alex.williamson@redhat.com>
2011-07-15 12:24:34 -0600

    VFIO based device assignment
    This patch adds qemu device assignment support using the proposed
    VFIO/UIOMMU kernel interfaces.  The existing KVM-only device assignment
    code makes use of various pci sysfs files for config space, MMIO BAR
    mapping, and misc other config items.  It then jumps over to KVM-specific
    ioctls for enabling interrupts and assigning devices to IOMMU domains.
    Finally, IO-port support uses either direct in/out or yet more PCI sysfs
    interfaces.  This is a messy model to support and causes numerous issues
    when we try to allow unprivileged users to access PCI devices.
    
    VFIO/UIOMMU reduces this to two interfaces, /dev/vfioX and /dev/uiommu.
    The VFIO device file provides all the necessary support for accessing
    PCI config space, read/write/mmap BARs (including IO-port space),
    configuring INTx/MSI/MSI-X interupts and setting up DMA mapping.  The
    UIOMMU interface allows iommu domains to be created, and via vfio,
    devices can be bound to a domain.  This provides an easier model to
    support and removes the bindings that make current device assignment
    only useable for KVM enabled guests.
    
    Usage is similar to KVM device assignment.  Rather than binding the
    device to the pci-stub driver, vfio devices need to be bound to the
    vfio driver.  From there, it's a simple matter of specifying the
    device as:
    
    -device vfio,host=01:00.0
    
    This example requires either root privileges or proper permissions on
    /dev/uiommu and /dev/vfioX.  To support unprivileged operation, the
    options vfiofd= and uiommufd= are available.  Depending on the usage
    of uiommufd, each guest device can be assigned to the same iommu
    domain, or to independent iommu domains.  In the example above, each
    device is assigned to a separate iommu domain.
    
    Signed-off-by: Alex Williamson <alex.williamson@redhat.com>

---
+++ hw/vfio.h
@@ -0,0 +1,90 @@
+#ifndef __VFIO_H__
+#define __VFIO_H__
+
+#include "qemu-common.h"
+#include "qemu-queue.h"
+#include "pci.h"
+#include "ioapic.h"
+
+typedef struct PCIHostDevice {
+    uint16_t seg;
+    uint8_t bus;
+    uint8_t dev:5;
+    uint8_t func:3;
+} PCIHostDevice;
+
+typedef struct PCIResource {
+    bool valid;
+    bool mem;
+    bool msix;
+    bool slow;
+    uint8_t bar;
+    size_t size;
+    ram_addr_t memory_index[2];  /* cpu_register_physical_memory() index */
+    void *r_virtbase[2];         /* mmapped address */
+    int io_mem;                  /* cpu_register_io_memory index */
+    pcibus_t e_phys;             /* emulated base address */
+    pcibus_t e_size;             /* emulated size of region in bytes */
+    uint32_t msix_offset;
+    int vfiofd;                  /* see vfio_resource_read/write */
+} PCIResource;
+
+typedef struct INTx {
+    bool pending;
+    uint8_t pin;
+    int irq;
+    EventNotifier interrupt;
+    Notifier eoi;
+    Notifier update_irq;
+} INTx;
+
+struct VFIODevice;
+
+typedef struct MSIVector {
+    EventNotifier interrupt;
+    struct VFIODevice *vdev;
+    int vector;
+} MSIVector;
+
+enum {
+    INT_NONE = 0,
+    INT_INTx = 1,
+    INT_MSI  = 2,
+    INT_MSIX = 3,
+};
+
+typedef struct VFIOUIOMMU {
+    int fd;
+    bool opened; /* Did we open fd, or was it opened for us? */
+    CPUPhysMemoryClient client;
+    QLIST_HEAD(, VFIODevice) vdevs;
+    QLIST_ENTRY(VFIOUIOMMU) next;
+} VFIOUIOMMU;
+
+typedef struct VFIODevice {
+    PCIDevice pdev;
+    int vfiofd;
+    INTx intx;
+    int msi_cap_size;
+    MSIVector *msi_vectors;
+    int nr_vectors;
+    int interrupt;
+    PCIResource resources[PCI_NUM_REGIONS - 1]; /* No ROM */
+    PCIHostDevice host;
+    VFIOUIOMMU *uiommu;
+    QLIST_ENTRY(VFIODevice) iommu_next;
+    QLIST_ENTRY(VFIODevice) nl_next;
+    QEMUTimer *remove_timer;
+    uint32_t flags;
+    char *vfiofd_name;
+    char *uiommufd_name;
+} VFIODevice;
+
+/* We can either create a domain per device or a domain per guest using
+ * the uiommu interface.  By default we set this bit true to share an
+ * iommu domain between devices for a guest.  This uses less resources
+ * in the host and eliminates extra physical memory clients for us. */
+#define VFIO_FLAG_UIOMMU_SHARED_BIT 0
+#define VFIO_FLAG_UIOMMU_SHARED (1U << VFIO_FLAG_UIOMMU_SHARED_BIT)
+
+#endif /* __VFIO_H__ */
